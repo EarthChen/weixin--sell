4005a5119d032ee2a9b393e0788c9dad2c48caa1
EarthChen <179153809@qq.com>
2017-12-27 16:01:05 +0800

    init

---
+++ src/main/java/com/earthchen/weixinsell/WeixinSellApplication.java
@@ -0,0 +1,13 @@
+package com.earthchen.weixinsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinSellApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinSellApplication.class, args);
+	}
+}
---
+++ src/test/java/com/earthchen/weixinsell/WeixinSellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.earthchen.weixinsell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeixinSellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
