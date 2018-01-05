07859d2b61badbc02b3140f56091c60db86c3051
EarthChen <179153809@qq.com>
2018-01-05 16:19:20 +0800

    卖家端商品和类目

---
+++ src/main/java/com/earthchen/weixinsell/form/CategoryForm.java
@@ -0,0 +1,15 @@
+package com.earthchen.weixinsell.form;
+
+import lombok.Data;
+
+@Data
+public class CategoryForm {
+
+    private Integer categoryId;
+
+    /** 类目名字. */
+    private String categoryName;
+
+    /** 类目编号. */
+    private Integer categoryType;
+}
